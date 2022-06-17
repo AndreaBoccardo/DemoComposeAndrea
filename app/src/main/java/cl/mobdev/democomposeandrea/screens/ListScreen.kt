d6302e088e859aaa9056f005b984cf9979d9bf09
Andrea Boccardo <andrea.boccardo@gmail.com>
2022-06-17 18:37:13 -0400

    Se empieza a implementar un scroll con la lista de cursos obtenida en la llamada al api

--- app/src/main/java/cl/mobdev/democomposeandrea/screens/ListScreen.kt
+++ app/src/main/java/cl/mobdev/democomposeandrea/screens/ListScreen.kt
@@ -1,13 +1,19 @@
 package cl.mobdev.democomposeandrea.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import cl.mobdev.democomposeandrea.model.CourseList
@@ -32,14 +38,23 @@ fun BodyContent(navController: NavController){
     val item = viewModel.listItem.value
     LazyColumn(){
 
+/*        items(item!!.size){course ->
+            MyItem(course)
+        }*/
     }
 }
 
 @Composable
 fun MyItem(course:CourseListItem){
-
-     Row() {
+     Row(
+         modifier = Modifier
+             .background(MaterialTheme.colors.background)
+             .padding(8.dp)
+     ) {
          AsyncImage(model = course.image, contentDescription = "")
-         Text(course.title!!)
+         Text(
+             course.title!!,
+             style = MaterialTheme.typography.h3
+         )
      }
 }
